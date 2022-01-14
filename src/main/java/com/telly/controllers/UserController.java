8afa9d572238d98ef84d05b6244f44764a610a0f
namgalff <hz_ziani@esi.dz>
2022-01-14 22:38:50 +0000

    Affichage des réservations

--- src/main/java/com/telly/controllers/UserController.java
+++ src/main/java/com/telly/controllers/UserController.java
@@ -23,7 +23,7 @@ public class UserController {
 
 	@Autowired
 	ReserveService reserveService;
-	
+
 	@RequestMapping(value = "/reservebook", method = RequestMethod.POST)
 	public String createReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, BindingResult result, Principal principal) {
 		
@@ -35,6 +35,22 @@ public class UserController {
 		reserve.getUser().setUsername(username);
 		
 		reserveService.reserve(reserve);
+	
+		
+		return "home";
+
+	}
+
+	@RequestMapping(value = "/getreservations", method = RequestMethod.GET)
+	public String getReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, Model model, Principal principal) {
+		
+		
+		String username = principal.getName();
+		reserve.getUser().setUsername(username);
+		
+		List<Reserve> reserves = reserveService.getReserves(username);
+		model.addAttribute("reserves", reserves);
+		System.out.println(reserves);
 	
 		
 		return "home";
